18aee70cac405414a00199a49247dfbf2a33ed00
physiocounterofc <pg7bfztj29@privaterelay.appleid.com>
2026-08-15 12:11:42 -0300

    Update spotify.js

--- spotify.js
+++ spotify.js
@@ -74,3 +74,38 @@ async function getRecentlyPlayed() {
         "/me/player/recently-played?limit=50"
     );
 }
+
+async function getCurrentlyPlaying() {
+    const token = getAccessToken();
+    
+    if (!token) {
+        throw new Error("Usuário não autenticado.");
+    }
+    
+    const response = await fetch(
+        "https://api.spotify.com/v1/me/player/currently-playing",
+        {
+            headers: {
+                Authorization: `Bearer ${token}`
+            }
+        }
+    );
+    
+    // 204 = nada está tocando
+    if (response.status === 204) {
+        return null;
+    }
+    
+    if (response.status === 401) {
+        logout();
+        throw new Error("Sessão expirada.");
+    }
+    
+    if (!response.ok) {
+        throw new Error(
+            `Spotify API error: ${response.status}`
+        );
+    }
+    
+    return await response.json();
+}
